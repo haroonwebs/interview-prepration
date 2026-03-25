79ccaa039a9c7ebeed1cf084c3fec6fbfe53d2ba
haroontalib <haroontalib8029@gmail.com>
2026-03-26 02:19:51 +0500

    even number

--- forLoop.cpp
+++ forLoop.cpp
@@ -3,13 +3,27 @@ using namespace std;
 
 
 // find the sum of the number from 1 to n
-int main(){
+// int main(){
+//     int n = 99;
+//     int sum=0;
+// for(int i=0; i<=n; i++){
+//     sum = sum + i;
+//  };
+//     cout << sum << endl;
+//     cout << endl;
+//  return 0;
+// };
+
+
+// find the even numbers of the number from 1 to n
+int main() {
     int n = 99;
-    int sum=0;
+
 for(int i=0; i<=n; i++){
-    sum = sum + i;
+    if(i%2==0){
+        cout << i << " ";
+    }
  };
-    cout << sum << endl;
     cout << endl;
 
  return 0;
