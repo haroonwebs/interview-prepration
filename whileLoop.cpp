631cce659d3429de2e67188711b4b517ba47a70e
haroontalib <haroontalib8029@gmail.com>
2026-03-26 01:47:07 +0500

    while loop

---
+++ whileLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+ int count = 1;
+ int n = 10;
+ while(count <= n){
+    cout << count << " ";
+    count++;
+ };
+
+cout << endl;
+
+ return 0;
+};
