9d558b6106caf889b421710f2a308e7ba30fa137
haroontalib <haroontalib8029@gmail.com>
2026-03-26 02:09:36 +0500

    find the sum of the number from 1 to n

--- forLoop.cpp
+++ forLoop.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 
+// find the sum of the number from 1 to n
 int main(){
-    int n = 100;
+    int n = 99;
+    int sum=0;
 for(int i=0; i<=n; i++){
-    cout << i << " ";
+    sum = sum + i;
  };
-
+    cout << sum << endl;
     cout << endl;
 
  return 0;
 };
+
+
