c2cb6b4c79dc0ff37976b7072232fff45c2abf1f
haroontalib <haroontalib8029@gmail.com>
2026-03-26 02:55:04 +0500

    factorial

--- forLoop.cpp
+++ forLoop.cpp
@@ -16,18 +16,32 @@ using namespace std;
 
 
 // find the even numbers of the number from 1 to n
-int main() {
-    int n = 99;
+// int main() {
+//     int n = 99;
 
-for(int i=0; i<=n; i++){
-    if(i%2==0){
-        cout << i << " ";
-    }
-    // to terminal the loop at specific point we use break statement 
-    if(i == 6){
-        break;
-    }
+// for(int i=0; i<=n; i++){
+//     if(i%2==0){
+//         cout << i << " ";
+//     }
+//     // to terminal the loop at specific point we use break statement 
+//     if(i == 6){
+//         break;
+//     }
+//  };
+//     cout << endl;
+
+//  return 0;
+// };
+
+
+// find the factorial numbers of the number
+int main() {
+    int n = 30;
+    int factorial = 1;
+for(int i=1; i<=n; i++){
+        factorial = factorial * i ;
  };
+    cout << factorial << endl;
     cout << endl;
 
  return 0;
