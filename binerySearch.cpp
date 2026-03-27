8ed31809bfbf3e81bcfc078db7f5c8554135e1f4
haroontalib <haroontalib8029@gmail.com>
2026-03-28 04:19:31 +0500

    implimented iterative bineryserch algorithm to find a specific number.

---
+++ binerySearch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// binery Search algothrim always implmented on the sorted arrays.
+int BinerySearch(vector<int> arr , int target){
+    int start=0 , end= arr.size()-1;
+    
+    while (start<=end){
+       int mid = (start + end)/2;
+       if(target > arr[mid]){
+        start = mid + 1;
+       }else if (target < arr[mid]){
+        end = mid - 1;
+       }else {
+         return mid;
+       }
+    };
+         
+       return -1;
+};
+
+
+
+
+
+int main (){
+    vector<int> arr = {1 , 3, 5, 6,7,9, 19, 20, 44, 64,654,7534};
+    int target1 = 7534;
+
+    cout << BinerySearch(arr, target1) << endl;
+
+}
