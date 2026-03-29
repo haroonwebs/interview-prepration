b01905c3d5335206f3032e28a49df5f7c8757e20
haroontalib <haroontalib8029@gmail.com>
2026-03-30 01:43:49 +0500

    added selection sort for decending order.

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // time complexity for the bubble sort is o(n^2)
-void SelectionSort(int arry[], int n){
+void SelectionSortForAssending(int arry[], int n){
   for (int i = 0; i < n-1; i++){
     int smallestIndx = i;
     for (int j = i+1; j < n; j++){
@@ -13,6 +13,19 @@ void SelectionSort(int arry[], int n){
     }
     swap(arry[i], arry[smallestIndx]);
   }
+};
+
+void SelectionSortForDecending(int arry[], int n){
+   for (int i = 0; i < n-1; i++){
+      int smallest = i; 
+            for (int j=i+1; j<=n-1; j++){
+                if(arry[j] > arry[smallest]){
+                    smallest = j;
+                }
+            }
+        swap(arry[i], arry[smallest]);
+   }
+   
 };
 
 
@@ -31,6 +44,7 @@ int main () {
   int arry[] = {1, 2, 3, 3, 8, 5 ,6 ,7};
   int n = sizeof(arry)/ sizeof(arry[0]);
 //   BubbleSortForDecending(arry, n);
-  SelectionSort(arry, n);
+//   SelectionSortForAssending(arry, n);
+  SelectionSortForDecending(arry, n);
   printSortedArry(arry, n);
 };
