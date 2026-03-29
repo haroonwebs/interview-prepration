4e43398ce9fb48238b8ef23b4157ff1b3a0492c7
haroontalib <haroontalib8029@gmail.com>
2026-03-29 22:32:04 +0500

    bubble sort for assending and decending

---
+++ bubbleSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void BubbleSortForAssending(int arry[], int n){
+  for (int i = 0; i < n-1; i++){
+    for (int j = 0; j < n-i-1; j++){
+        if(arry[j] > arry[j+1]){
+            swap(arry[j], arry[j+1]);
+        }
+    }
+  };
+};
+
+void BubbleSortForDecending(int arry[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++){
+            if(arry[j] < arry[j+1]){
+                swap(arry[j], arry[j+1]);
+            }
+        }
+        
+    }
+    
+}
+
+void printSortedArry(int arry[], int n){
+    for (int i = 0; i < n-1; i++){
+        cout << arry[i] << " ";
+    };
+};
+
+
+
+
+
+int main () {
+  int arry[] = {1, 5, 6,3, 2, 6 ,2 , 6};
+  int n = sizeof(arry)/ sizeof(arry[0]);
+  BubbleSortForDecending(arry, n);
+//   BubbleSortForAssending(arry, n);
+  printSortedArry(arry, n);
+};
