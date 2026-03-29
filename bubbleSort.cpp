56af5ddd23251a077d64725463049e2fc389a4c7
haroontalib <haroontalib8029@gmail.com>
2026-03-29 22:54:44 +0500

    added a minor optimization

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,28 +5,37 @@ using namespace std;
 // time complexity for the bubble sort is o(n^2)
 void BubbleSortForAssending(int arry[], int n){
   for (int i = 0; i < n-1; i++){
+    bool isSwap = false;
     for (int j = 0; j < n-i-1; j++){
         if(arry[j] > arry[j+1]){
             swap(arry[j], arry[j+1]);
+            isSwap = true;
         }
+    }
+    if(!isSwap){ // array is already swaped.
+        return ;
     }
   };
 };
 
 void BubbleSortForDecending(int arry[], int n){
     for (int i = 0; i < n-1; i++){
+        bool isSwap = false;
         for (int j = 0; j < n-i-1; j++){
             if(arry[j] < arry[j+1]){
                 swap(arry[j], arry[j+1]);
+                isSwap = true;
             }
         }
-        
+      if(!isSwap){
+        return;
+      }
     }
     
 }
 
 void printSortedArry(int arry[], int n){
-    for (int i = 0; i < n-1; i++){
+    for (int i = 0; i <= n-1; i++){
         cout << arry[i] << " ";
     };
 };
@@ -36,9 +45,9 @@ void printSortedArry(int arry[], int n){
 
 
 int main () {
-  int arry[] = {1, 5, 6,3, 2, 6 ,2 , 6};
+  int arry[] = {1, 2, 3, 3, 8, 5 ,6 ,7};
   int n = sizeof(arry)/ sizeof(arry[0]);
-  BubbleSortForDecending(arry, n);
-//   BubbleSortForAssending(arry, n);
+//   BubbleSortForDecending(arry, n);
+  BubbleSortForAssending(arry, n);
   printSortedArry(arry, n);
 };
